8b17df7d71b3e5b517384692754f841c9c088bc0
firecrow <fire@firecrow.com>
2018-08-27 16:41:24 -0400

    add_at feature working

--- crray.h
+++ crray.h
@@ -9,37 +9,38 @@ struct crray {
 	void **items;
 };
 
-enum CRRAY_STATUS { CRRAY_OK, CRRAY_NOT_FOUND };
+enum CRRAY_STATUS { CRRAY_OK, CRRAY_NOT_FOUND, CRRAY_BOUNDS_ERROR };
 typedef int CRRAY_IDX;
 
 int resize_if_(struct crray *arr, int size){
 	int newsize;
-	if(arr->allocated > 0) {
-		newsize = arr->allocated;
-		while((newsize = newsize*2*2) < size){}
-	}else{
-		newsize = size;	
-	}
 	void *new = NULL;
 	if(arr->allocated < size){
-		new = (void *)malloc(sizeof(void *)*newsize);
-		if(!new){
-			printf("oops no memory");
-			exit(1);
+		if(arr->allocated > 0) {
+			newsize = arr->allocated;
+			while((newsize = newsize*2*2) < size){}
+		}else{
+			newsize = size;	
 		}
+			new = (void *)malloc(sizeof(void *)*newsize);
+			if(!new){
+				printf("oops no memory\n");
+				exit(1);
+			}
+		if(arr->length != 0){
+			memcpy(new, arr->items, sizeof(void *)*arr->length);
+			free(arr->items);
+		}
+		arr->items = new;
+		return newsize;
 	}
-	if(arr->length != 0){
-		memcpy(new, arr->items, sizeof(void *)*arr->length);
-		free(arr->items);
-	}
-	arr->items = new;
-	return newsize;
+	return arr->allocated;
 }
 
 struct crray *crray_init(){
 	struct crray *arr = (struct crray *)malloc(sizeof(struct crray));
 	if(!arr){
-		printf("oops no memory");
+		printf("oops no memory\n");
 		exit(1);
 	}
 	arr->length = 0;
@@ -53,15 +54,35 @@ CRRAY_IDX add(struct crray *arr, void *item){
 }
 
 enum CRRAY_STATUS set(struct crray *arr, void *item, int idx){
-   
 }
 
 CRRAY_IDX add_at(struct crray *arr, void *item, int idx){
-   
+	printf("hi 1\n");
+	if(idx == -1){
+		return add_at(arr, item, arr->length);
+	}
+	printf("hi 2\n");
+	if(idx > arr->length){
+		return CRRAY_BOUNDS_ERROR;
+	}
+	printf("hi 3\n");
+	arr->allocated = resize_if_(arr, arr->length);
+        if(idx != arr->length){
+		printf("hi 4 not supposed to be here\n");
+		memcpy(arr->items+idx, arr->items+(idx-1), sizeof(void *)*(arr->length-idx+1));
+	}
+	arr->items[idx] = item;
+	arr->length++;
+	printf("hi 5\n");
+	return idx;
 }
 
-enum CRRAY_STATUS get(struct crray *arr, void *item, int idx, void **result){
-   
+enum CRRAY_STATUS get(struct crray *arr, int idx, void **result){
+	if(idx < 0 || idx > arr->length-1){
+		return CRRAY_BOUNDS_ERROR;
+	}
+	*result = arr->items[idx];
+	return CRRAY_OK;
 }
 
 enum CRRAY_STATUS pop(struct crray *arr, void *item, int idx, void **result){
--- test.c
+++ test.c
@@ -3,4 +3,44 @@
 
 int main(int argc, char **argv){
 	printf("hi\n");
+
+	struct crray *arr = crray_init();
+	printf("alloc:%d length:%d\n", arr->allocated, arr->length);
+
+	char alpha[] = "alpha";
+	char bravo[] = "bravo";
+
+	int a = add(arr, &alpha);
+	printf("a:%d\n", a);
+	int b = add(arr, &bravo);
+	printf("b:%d\n", b);
+	printf("alloc:%d length:%d\n", arr->allocated, arr->length);
+
+	char *hiback;
+	get(arr, 0, &hiback);
+	printf("result from 0 :%s\n", hiback);
+
+	char *pooback;
+	get(arr, 1, &pooback);
+	printf("result from 1 :%s\n", pooback);
+
+
+	char charlie[] = "charlie"; 
+	int c = add_at(arr, &charlie, 1);
+	printf("alloc:%d length:%d\n", arr->allocated, arr->length);
+
+	char *aback1;
+	get(arr, 0, &aback1);
+	printf("result from 0 :%s\n", aback1);
+
+	char *aback2;
+	get(arr, 1, &aback2);
+	printf("result from 1 :%s\n", aback2);
+
+	char *aback3;
+	get(arr, 2, &aback3);
+	printf("result from 1 :%s\n", aback3);
+
 }
+
+
