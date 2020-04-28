c10731d9e84ea12cd759e3341633cc408d7575c2
firecrow <fire@firecrow.com>
2020-04-28 00:13:48 -0400

    sketching out pop function

--- ptrarray.c
+++ ptrarray.c
@@ -5,6 +5,13 @@ struct ptrarray {
     size_t alloc_length;
 };
 
+void ptrarray_free(void *arr){
+    int i; 
+    for(i=0; i< arr->length; i++){
+        ref_decr(arr->content[i]);
+    }
+    free(arr);
+}
 
 struct ptrarray *ptrarray_alloc(size_t initial_size){
     struct ptrarray *arr;
@@ -13,6 +20,8 @@ struct ptrarray *ptrarray_alloc(size_t initial_size){
     if(initial_size < 32) initial_size = 32;
     arr->content = malloc(initial_size);
     arr->alloc_length = initial_size;
+    arr->base.class = CLASSES_PTRARRAY;
+    arr->base.free = ptrarray_free;
     return arr;
 }
 
@@ -23,5 +32,9 @@ void arr_push(struct ptrarray *arr, struct abstract *obj, int idx){
     }
     if(idx == -1) idx = arr->length;
     else memmove(arr->content[idx+1], arr->content[idx], app->length-idx);
-    arr->content[idx] = obj;
+    arr->content[idx] = ref_incr(obj);
+}
+
+void arr_pop(struct ptrarray *arr, struct abstract *obj, int idx){
+    memmove(arr->content[idx], arr->content[idx+1], app->length-idx);
 }
