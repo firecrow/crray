6dea4adf44a681a8a12f01ab814a69a7ff894237
firecrow <fire@firecrow.com>
2020-05-02 20:07:17 -0400

    fixed tests

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CC=clang-9
 all:
-	$(CC) -o crray.test test.c 
+	$(CC) -o test test.c 
 
--- test.c
+++ test.c
@@ -17,8 +17,7 @@ void print_out(Crray *arr){
 }
 
 int main(){
-    /*
-  struct ptrarray *arr = ptrarray_alloc(1);
+  Crray *arr = crray_alloc(1);
   char alpha[]="alcahol is nice";
   char bravo[]="beetles are nice and crunchy";
   char charlie[]="carrots and canalope";
@@ -27,26 +26,26 @@ int main(){
   char foxtrot[]="freedom is over rated";
   char golf[]="green tomatoes and ham";
 
-  arr_push(arr, (struct abstract *)alpha, -1);
+  crray_push(arr, (struct abstract *)alpha, -1);
   handle_case(arr->length == 1, "first item has length 1", 
     arr->length, "");
   handle_case(arr->alloc_length == 4, "alloc size 4", 
     arr->alloc_length, "");
 
-  arr_push(arr, (struct abstract *)bravo, -1);
-  arr_push(arr, (struct abstract *)charlie, -1);
-  arr_push(arr, (struct abstract *)delta, -1);
-  arr_push(arr, (struct abstract *)echo, -1);
+  crray_push(arr, (struct abstract *)bravo, -1);
+  crray_push(arr, (struct abstract *)charlie, -1);
+  crray_push(arr, (struct abstract *)delta, -1);
+  crray_push(arr, (struct abstract *)echo, -1);
   handle_case(arr->length == 5, "fifth item has length 5", 
     arr->length, "");
   handle_case(arr->alloc_length == 8, "alloc size 8", 
     arr->alloc_length, "");
   printf("\x1b[0m");
 
-  arr = ptrarray_alloc(1);
-  arr_push(arr, (struct abstract *)alpha, -1);
-  arr_push(arr, (struct abstract *)bravo, -1);
-  arr_push(arr, (struct abstract *)charlie, 1);
+  arr = crray_alloc(1);
+  crray_push(arr, (struct abstract *)alpha, -1);
+  crray_push(arr, (struct abstract *)bravo, -1);
+  crray_push(arr, (struct abstract *)charlie, 1);
   print_out(arr);
   handle_case(arr->length == 3, "after insert idx1 length 3", 
     arr->length, "");
@@ -58,16 +57,15 @@ int main(){
   handle_case(arr->content[2] == bravo, "second elem elem is third", 
     arr->length, arr->content[2]);
 
-  arr_push(arr, (struct abstract *)delta, -1);
-  arr_push(arr, (struct abstract *)echo, -1);
-  arr_push(arr, (struct abstract *)foxtrot, -1);
-  arr_push(arr, (struct abstract *)golf, -1);
+  crray_push(arr, (struct abstract *)delta, -1);
+  crray_push(arr, (struct abstract *)echo, -1);
+  crray_push(arr, (struct abstract *)foxtrot, -1);
+  crray_push(arr, (struct abstract *)golf, -1);
   print_out(arr);
   printf("remove 3\n");
-  arr_remove(arr, 3);
+  crray_remove(arr, 3);
   print_out(arr);
   printf("remove 0\n");
-  arr_remove(arr, 0);
+  crray_remove(arr, 0);
   print_out(arr);
-  */
 }
