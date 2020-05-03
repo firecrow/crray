5087cf09bc6b0784eed082f2c08a865cb02130b1
firecrow <fire@firecrow.com>
2020-05-02 23:33:21 -0400

    upated with prefixes

--- crray.c
+++ crray.c
@@ -8,72 +8,71 @@ typedef struct crrray {
     size_t alloc_length;
 } Crray;
 
-void crray_free(void *_arr){
+static void ctl_crray_free(void *_arr){
     Crray *arr = (Crray *)_arr;
     int i; 
     for(i=0; i< arr->length; i++){
-        ref_decr(arr->content[i]);
+        ctl_ref_decr(arr->content[i]);
     }
     free(arr);
 }
 
-Crray *crray_alloc(size_t initial_size){
+Crray *ctl_crray_alloc(size_t initial_size){
     Crray *arr;
-    xokptr(arr = malloc(sizeof(Crray)));
+    ctl_xptr(arr = malloc(sizeof(Crray)));
     bzero(arr, sizeof(Crray));
     if(initial_size < BLOCK_SIZE) initial_size = BLOCK_SIZE;
-    xokptr(arr->content = malloc(initial_size*sizeof(void *)));
+    ctl_xptr(arr->content = malloc(initial_size*sizeof(void *)));
     arr->alloc_length = initial_size;
     arr->base.class = CLASS_PTRARRAY;
-    arr->base.free = crray_free;
+    arr->base.free = ctl_crray_free;
     return arr;
 }
 
-void crray_push(Crray *arr, Abs *obj, int idx){
+void ctl_crray_push(Crray *arr, CtlAbs *obj, int idx){
     if(arr->length+1 > arr->alloc_length){
         arr->alloc_length = arr->alloc_length*2;
-        xokptr(arr->content = realloc(arr->content, arr->alloc_length*sizeof(void *)));
+        ctl_xptr(arr->content = realloc(arr->content, arr->alloc_length*sizeof(void *)));
     }
     if(idx == -1) idx = arr->length;
     else memmove(arr->content+(idx+1), arr->content+idx, (arr->length-idx)*sizeof(void*));
-    /*arr->content[idx] = ref_incr(obj);*/
-    arr->content[idx] = obj;
+    arr->content[idx] = ctl_ref_incr(obj);
     arr->length++;
 }
 
-void crray_remove(Crray *arr, int idx){
-    ref_decr(arr->content[idx]);
+void ctl_crray_remove(Crray *arr, int idx){
+    ctl_ref_decr(arr->content[idx]);
     memmove(arr->content+idx, arr->content+(idx+1), (arr->length-idx)*sizeof(void *));
     arr->length--;
 }
 
-Crray *split(Counted *str, Counted *sep){
+Crray *ctl_split(CtlCounted *str, CtlCounted *sep){
     char *p  = str->data;
     char *b  = p;
     char *s = sep->data;
-    Crray *arr = crray_alloc(0);
+    Crray *arr = ctl_crray_alloc(0);
     while(p-str->data < str->length){
         if(*p++ == *s){ s++;
         } else s = sep->data;
         if(s-sep->data == sep->length){
-            crray_push(arr, (Abs *)counted_alloc(b, p-b-sep->length), -1);
+            ctl_crray_push(arr, (CtlAbs *)ctl_counted_alloc(b, p-b-sep->length), -1);
             b = p;
             s = sep->data;
         }
     }
     if(b != p){
-        crray_push(arr, (Abs *)counted_alloc(b, p-b), -1);
+        ctl_crray_push(arr, (CtlAbs *)ctl_counted_alloc(b, p-b), -1);
     }
     return arr;
 }
 
-Counted *join(Crray *arr, Counted *sep){
-    Counted *c = clone((Counted *)arr->content[0]);
-    Counted *y;
+CtlCounted *ctl_join(Crray *arr, CtlCounted *sep){
+    CtlCounted *c = clone((CtlCounted *)arr->content[0]);
+    CtlCounted *y;
     for(int i = 1; i< arr->length; i++) { 
-        y = (Counted *)arr->content[i];
-        counted_push(c, sep->data, sep->length);
-        counted_push(c, y->data, y->length);
+        y = (CtlCounted *)arr->content[i];
+        ctl_counted_push(c, sep->data, sep->length);
+        ctl_counted_push(c, y->data, y->length);
     }
     return c;
 }
--- test.c
+++ test.c
@@ -19,14 +19,14 @@ void print_out(Crray *arr){
 void print_out_counted(Crray *arr){
   printf("-------- start -------------\n");
   for(int i=0;i<arr->length;i++){
-    Counted *c = arr->content[i];
+    CtlCounted *c = arr->content[i];
     write(1, c->data, c->length); 
     write(1, "\n", 1); 
   }
 }
 
 int main(){
-  Crray *arr = crray_alloc(1);
+  Crray *arr = ctl_crray_alloc(1);
   char alpha[]="alcahol is nice";
   char bravo[]="beetles are nice and crunchy";
   char charlie[]="carrots and canalope";
@@ -35,26 +35,26 @@ int main(){
   char foxtrot[]="freedom is over rated";
   char golf[]="green tomatoes and ham";
 
-  crray_push(arr, (Abs *)alpha, -1);
+  ctl_crray_push(arr, (CtlAbs *)alpha, -1);
   handle_case(arr->length == 1, "first item has length 1", 
     arr->length, "");
   handle_case(arr->alloc_length == 4, "alloc size 4", 
     arr->alloc_length, "");
 
-  crray_push(arr, (Abs *)bravo, -1);
-  crray_push(arr, (Abs *)charlie, -1);
-  crray_push(arr, (Abs *)delta, -1);
-  crray_push(arr, (Abs *)echo, -1);
+  ctl_crray_push(arr, (CtlAbs *)bravo, -1);
+  ctl_crray_push(arr, (CtlAbs *)charlie, -1);
+  ctl_crray_push(arr, (CtlAbs *)delta, -1);
+  ctl_crray_push(arr, (CtlAbs *)echo, -1);
   handle_case(arr->length == 5, "fifth item has length 5", 
     arr->length, "");
   handle_case(arr->alloc_length == 8, "alloc size 8", 
     arr->alloc_length, "");
   printf("\x1b[0m");
 
-  arr = crray_alloc(1);
-  crray_push(arr, (Abs *)alpha, -1);
-  crray_push(arr, (Abs *)bravo, -1);
-  crray_push(arr, (Abs *)charlie, 1);
+  arr = ctl_crray_alloc(1);
+  ctl_crray_push(arr, (CtlAbs *)alpha, -1);
+  ctl_crray_push(arr, (CtlAbs *)bravo, -1);
+  ctl_crray_push(arr, (CtlAbs *)charlie, 1);
   print_out(arr);
   handle_case(arr->length == 3, "after insert idx1 length 3", 
     arr->length, "");
@@ -66,55 +66,55 @@ int main(){
   handle_case(arr->content[2] == bravo, "second elem elem is third", 
     arr->length, arr->content[2]);
 
-  crray_push(arr, (Abs *)delta, -1);
-  crray_push(arr, (Abs *)echo, -1);
-  crray_push(arr, (Abs *)foxtrot, -1);
-  crray_push(arr, (Abs *)golf, -1);
+  ctl_crray_push(arr, (CtlAbs *)delta, -1);
+  ctl_crray_push(arr, (CtlAbs *)echo, -1);
+  ctl_crray_push(arr, (CtlAbs *)foxtrot, -1);
+  ctl_crray_push(arr, (CtlAbs *)golf, -1);
   print_out(arr);
   printf("remove 3\n");
-  crray_remove(arr, 3);
+  ctl_crray_remove(arr, 3);
   print_out(arr);
   printf("remove 0\n");
-  crray_remove(arr, 0);
+  ctl_crray_remove(arr, 0);
   print_out(arr);
 
-  arr = crray_alloc(0);
-  crray_push(arr, (Abs *)counted_from_cstr("apple"), -1);
-  crray_push(arr, (Abs *)counted_from_cstr("bananna"), -1);
-  crray_push(arr, (Abs *)counted_from_cstr("carrot"), -1);
-  Counted *sep = counted_from_cstr(", ");
+  arr = ctl_crray_alloc(0);
+  ctl_crray_push(arr, (CtlAbs *)ctl_counted_from_cstr("apple"), -1);
+  ctl_crray_push(arr, (CtlAbs *)ctl_counted_from_cstr("bananna"), -1);
+  ctl_crray_push(arr, (CtlAbs *)ctl_counted_from_cstr("carrot"), -1);
+  CtlCounted *sep = ctl_counted_from_cstr(", ");
 
-  Counted *out = join(arr, sep);
-  char join_out[] = "apple, bananna, carrot";
-  int out_r = strcmp(join_out, out->data);
+  CtlCounted *out = ctl_join(arr, sep);
+  char ctl_join_out[] = "apple, bananna, carrot";
+  int out_r = strcmp(ctl_join_out, out->data);
   handle_case(out_r == 0, "join results", out_r, "");
 
-  Crray *splitout = split(counted_from_cstr("./usr/../local/../this/../../is/../rediculous"), counted_from_cstr("../"));
+  Crray *splitout = ctl_split(ctl_counted_from_cstr("./usr/../local/../this/../../is/../rediculous"), ctl_counted_from_cstr("../"));
   handle_case(splitout->length == 6, "split by ../ results ", splitout->length, "");
 
-  out_r = strcmp("./usr/", ((Counted *)splitout->content[0])->data);
+  out_r = strcmp("./usr/", ((CtlCounted *)splitout->content[0])->data);
   handle_case(out_r == 0, "frst split results ", out_r, "");
 
-  out_r = strcmp("local/", ((Counted *)splitout->content[1])->data);
+  out_r = strcmp("local/", ((CtlCounted *)splitout->content[1])->data);
   handle_case(out_r == 0, "second split results ", out_r, "");
 
-  out_r = strcmp("this/", ((Counted *)splitout->content[2])->data);
+  out_r = strcmp("this/", ((CtlCounted *)splitout->content[2])->data);
   handle_case(out_r == 0, "third split results ", out_r, "");
 
-  handle_case(((Counted *)splitout->content[3])->length == 0, "fourth split results ", ((Counted *)splitout->content[3])->length, "");
+  handle_case(((CtlCounted *)splitout->content[3])->length == 0, "fourth split results ", ((CtlCounted *)splitout->content[3])->length, "");
 
-  out_r = strcmp("is/", ((Counted *)splitout->content[4])->data);
+  out_r = strcmp("is/", ((CtlCounted *)splitout->content[4])->data);
   handle_case(out_r == 0, "fifth split results ", out_r, "");
 
-  out_r = strcmp("rediculous", ((Counted *)splitout->content[5])->data);
+  out_r = strcmp("rediculous", ((CtlCounted *)splitout->content[5])->data);
   handle_case(out_r == 0, "sixth split results ", out_r, "");
 
-  splitout = split(counted_from_cstr("one/two/three"), counted_from_cstr("/"));
+  splitout = ctl_split(ctl_counted_from_cstr("one/two/three"), ctl_counted_from_cstr("/"));
   print_out_counted(splitout);
-  out_r = strcmp("one", ((Counted *)splitout->content[0])->data);
+  out_r = strcmp("one", ((CtlCounted *)splitout->content[0])->data);
   handle_case(out_r == 0, "frst simple / split results ", out_r, "");
-  out_r = strcmp("two", ((Counted *)splitout->content[1])->data);
+  out_r = strcmp("two", ((CtlCounted *)splitout->content[1])->data);
   handle_case(out_r == 0, "frst simple / split results ", out_r, "");
-  out_r = strcmp("three", ((Counted *)splitout->content[2])->data);
+  out_r = strcmp("three", ((CtlCounted *)splitout->content[2])->data);
   handle_case(out_r == 0, "frst simple / split results ", out_r, "");
 }
